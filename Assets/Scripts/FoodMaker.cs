de47922c379f202835718dda1588c5bc036dfcc7
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-09-22 23:44:57 +0900

    Instantiate ui stick
    ui 표시용 제작 막대 오브젝트 생성

---
+++ Assets/Scripts/FoodMaker.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodMaker : MonoBehaviour
+{
+    // 막대 생성 좌표 저장 오브젝트
+    public GameObject stickPosObj;
+    // 막대 오브젝트 프리팹
+    public GameObject stickPref;
+
+    GameObject stick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        // 활성화 시 화면에 막대 생성
+        Debug.Log("막대 생성");
+        stick = Instantiate(stickPref, stickPosObj.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DestroyStick()
+    {
+        DestroyImmediate(stick);
+    }
+}
