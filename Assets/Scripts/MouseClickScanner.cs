55ef0d410ec7e6fb95dfd8cb612a5a9aa09c32fe
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-09-22 23:27:01 +0900

    Make ui on/off
    제작 ui on/off 기능 구현

---
+++ Assets/Scripts/MouseClickScanner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// 마우스 클릭을 감지하는 클래스
+public class MouseClickScanner : MonoBehaviour
+{
+    // 마우스 이벤트 트리거
+    public bool mouseEventEnable = true;
+
+    public UnityEvent mouseClikedEvent;
+
+    // 오브젝트 클릭시 발동하는 유니티 이벤트함수
+    public void OnMouseDown()
+    {
+        // 메인 카메라 화면의 스크린 좌표계 기반으로 현재 마우스 좌표에서의 ray 선언
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        // ray 방향으로 Raycast 수행. 
+        if (Physics.Raycast(ray, out raycastHit) && mouseEventEnable) // ray가 물체와 충돌할 + 트리거 활성화일 경우
+        {
+            Debug.Log("이벤트");
+            if (raycastHit.transform.tag == "Event_Plate")
+            {
+                mouseEventEnable = false;
+                mouseClikedEvent.Invoke();
+            }
+        }
+    }
+
+    public void InitScanner()
+    {
+        mouseEventEnable = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
